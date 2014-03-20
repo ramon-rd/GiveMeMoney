516659e7ba8295f5b713a4fbc426f9280801df5f
STiago <victoriasantiago@correo.ugr.es>
2014-03-20 16:11:54 +0100

    con formularios

--- GiveMeMoney/web/givememoney.dart
+++ GiveMeMoney/web/givememoney.dart
@@ -1,9 +1,38 @@
 import 'dart:html';
+/*import 'package:js/js.dart' as js;*/
+/*
+void drawChart() {
+    var gviz= js.context.google.visualization;
+    
+        var data = [
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ];
+        
+        var arrayData = js.array(data);
+        var tableData = gviz.arrayToDataTable(arrayData);
+        
+        var options = js.map({
+                 'title': 'Company Performance',
+                 'hAxis': {'title': 'Year', 'titleTextStyle': {'color': 'red'}}
+               });
+
+         var chart = new js.Proxy(gviz.ColumnChart, querySelector('#isualization'));
+          chart.draw(tableData, options);
+}*/
 
 void main() {
   /*querySelector("#sample_text_id")
       ..text = "Click me!"
       ..onClick.listen(reverseText);*/
+ /* js.context.google.load('visualization', '1', js.map(
+     {
+       'packages': ['corechart'],
+       'callback': drawChart,
+       }));*/
 }
 
 /*void reverseText(MouseEvent event) {
