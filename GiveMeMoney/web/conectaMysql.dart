de885efc9406792c3a456304edae7a77cfcc331f
Luisre <luis.redondo.exposito@gmail.com>
2014-04-03 10:39:45 +0200

    sql limpio

--- GiveMeMoney/web/conectaMysql.dart
+++ GiveMeMoney/web/conectaMysql.dart
@@ -1,166 +1,155 @@
-//import 'package:sqljocky/sqljocky.dart';
-//import 'package:sqljocky/utils.dart';
+//library GiveMeMoney;
+
+import 'package:sqljocky/sqljocky.dart';
+import 'package:sqljocky/utils.dart';
+
 import 'dart:async';
 import 'GastoDiario.dart';
-import 'dart:io';
-import 'dart:convert';
-
-
 
 class ManejaSQL {
   
   ConnectionPool pool;
   
   ManejaSQL(this.pool);
-  
-  Future run() {
+
+//Metodos limpios.
+//*****************
+
+//Insertar nuevo alumno en BD.
+
+  Future addUsuario(nombre_user, nombre, apellidos, email, password) {
+    
     var completer = new Completer();
-
-
-    dropTables().then((_) {
-      print("dropped tables");
-      // then recreate the tables
-      return createTables();
-    }).then((_) {
-      print("created tables");
-      // add some data
-      return addData();
-    }).then((_) {
-      // and read it back out
-
-      return readData();
-    }).then((_) {
-      completer.complete(null);
+    
+    pool.query('insert into usuario (nombre_user, nombre, apellidos, email, password) values ("$nombre_user", "$nombre", "$apellidos", "$email", "$password")').then((query){
+      print("Consulta preparada...");
     });
+    
+    print("Usuario insertado.");
     return completer.future;
   }
-
-
-  Future addDataIngreso(valor, fecha) {
-
-      var completer = new Completer();
-      pool.prepare("insert into ingreso (valor, fecha_ingreso) values (?, ?)").then((query){
-      print("Preparando consulta insertar ingreso...");
-      var parameters = [
-            [valor, fecha]];
-      return query.executeMulti(parameters);
-    }).then((results) {
-      print("Ejecutar consulta (insertar ingreso)");
-      completer.complete(null);
-    });
-      print("insertado");
-      
-      return completer.future;
-      
-    }
   
+//Leer un usuario de la BD.
   
-  Future addData(saldo_nuevo,nombre) {
+  Future readUsuario(nombre_user) {
     
     var completer = new Completer();
-    pool.query('update usuario set saldo="$saldo_nuevo" where nombre_user="$nombre"').then((query){
-    print("Preparando consulta insertar usuario...");
-  /*  return query.executeMulti(parameters);
-  }).then((results) {
-    print("Ejecutar consulta (insertar)");
-    completer.complete(null);*/
-  });
-  /*  print("insertado");
-    return completer.future;*/
+    
+    pool.query('select * from usuario where nombre_user = "$nombre_user"').then((query) {
+      print("Consulta preparada...");
+      query.forEach((row) {
+        print("Nombre de usuario: ${row[0]} \nNombre: ${row[1]} \nApellidos: ${row[2]} \nEmail: ${row[3]} \nPassword: ${row[4]}");
+      });
+    });
+    
+    print("Usuario consultado.");
     return completer.future;
-    
   }
-   
-  Future addDataCategoria(valor, fecha, categoria) {
-
-      var completer = new Completer();
-      pool.prepare("insert into $categoria (valor, fecha) values (?, ?)").then((query){
-      print("Preparando consulta insertar gasto en categoria...");
-      var parameters = [
-            [valor, fecha]];
-      return query.executeMulti(parameters);
-    }).then((results) {
-      print("Ejecutar consulta (insertar $categoria)");
-      completer.complete(null);
-    });
-      print("insertado");
-      return completer.future;
-      
-}  
   
-  Future readData(nombre) {
+//Establecer saldo de un usuario.
+  
+  Future setSaldoUsuario(nombre_user, saldo) {
     
     var completer = new Completer();
-       print("querying");
-       return pool.query('select saldo '
-           'from usuario u '
-           'where u.nombre_user = "$nombre"').then((result) {
-           //  print(result.elementAt(4));
-             
-         return result.forEach((row) {
-           print("Saldo: ${row[0]}");
-         /*
-           if (row[3] == null) {
-             print("ID: ${row[0]}, Name: ${row[1]}, Age: ${row[2]}, No Pets");
-           } else {
-             print("ID: ${row[0]}, Name: ${row[1]}, Age: ${row[2]}, Pet Name: ${row[3]}, Pet Species ${row[4]}");
-           }*/
-         });
-       });
     
+    pool.query('update usuario set saldo = "$saldo" where nombre_user = "$nombre_user"').then((result){
+      print("Consulta preparada...");
+    });
+    
+      print("Saldo insertado.");
+      return completer.future;
   }
+  
+//Obtener el saldo de un usuario.
+  
+  Future getSaldoUsuario(nombre_user) {
+    
+    var completer = new Completer();
+    
+    pool.query('select saldo from usuario where nombre_user = "$nombre_user"').then((query) {
+      print("Consulta preparada...");
+      query.forEach((row){
+        print("Saldo: ${row[0]}");
+      });
+    });
+    
+    print("Saldo consultado.");
+    return completer.future;   
+  }
+  
+//Insertar nuevo ingreso en BD.
+  
+  Future addIngreso(valor) {
+      
+      var completer = new Completer();
+      
+      DateTime fecha_ingreso = new DateTime.now();
+      
+      pool.query('insert into ingreso (valor, fecha_ingreso) values ("$valor", "$fecha_ingreso")').then((query){
+        print("Consulta preparada...");
+      });
+      
+      print("Usuario insertado.");
+      return completer.future;
+  }
+  
+//Leer un ingreso de la BD.
+  
+  Future readIngreso() {
+    
+    var completer = new Completer();
+    
+    pool.query('select * from ingreso').then((query){
+      print("Consulta preparada...");
+      query.forEach((row){
+        print("ID: ${row[0]} \nValor: ${row[1]} \nFecha: ${row[2]}");
+      });
+    });
+        
+    print("Usuario insertado.");
+    return completer.future;
+  
+  }
+  
+//Insertar nuevo gasto en BD.
+  
+  Future addGasto(){
+    
+    var completer = new Completer(descripcion);
+    
+    pool.query('insert into gasto (descripcion) values ("$descripcion")')
+  }
+    
+ 
 }
+
 /*
-const String webHost = "127.0.0.1";
-const int webPuerto = 6666;
-var contenidos;
 
 void main() {
+
+  //Configuración
   
-  HttpServer.bind(webHost, webPuerto).then((server){
-    server.listen((HttpRequest request){
-     request.listen((List<int> buffer){
-       var jsonString = new String.fromCharCodes(buffer);
-       Map data = JSON.decode(jsonString);
-       print(data[0]);
-       print(data[1]);
-       switch(request.uri.path){
-         case "/login":
-           //registro en bd
-           //contenidos = login(data);
-           contenidos = JSON.encode(contenidos);
-           data = {};
-           break;
-         
-         default:
-           print("default");
-           break;
-       }
-     });
-     Responde(request);
-    });
+      String user = "root";
+      String password = "09l09r88e";
+      int port = 3306;
+      String db = "givememoney";
+      String host = "localhost";
+      print("Configuración aceptada");
+      
+//ConnectionPool pool = new ConnectionPool(host: "localhost", port: 3306, user: "root", password: "09l09r88e", db: "givememoney", max:1);
+
+  print("opening connection");
+  var pool = new ConnectionPool(host: host, port: port, user: user, password: password, db: db, max:1);
+  print("connection open");
+
+  
+  var example = new ManejaSQL(pool);
+
+  print("running example");
+  example.run().then((_) {
+
+   
+    print("K THNX BYE!");
+    pool.close();
   });
-  
-}
-
-void Responde(HttpRequest request){
-  if (contenidos != null){
-    request.response
-      ..headers.add('Acces-Control-Allow-Origin', '*')
-      ..headers.add('Content-Type','application/x-www-form-urlencoded')
-      ..headers.add('Acept')
-      ..statusCode = 201
-      ..write(contenidos)
-      ..close();
-  }else{
-    request.response
-          ..headers.add('Acces-Control-Allow-Origin', '*')
-          ..headers.add('Content-Type','text/plain')
-          ..statusCode = 500
-          ..write("Error")
-          ..close();
-  } 
-}
-*/
-
-
+}*/
