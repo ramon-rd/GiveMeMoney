6c68baf4a62b7248e6ded262602b8060fb6ba364
jcastilloprez <jcastilloprez@correo.ugr.es>
2014-04-10 19:39:15 +0200

    Corregidos varios errores

--- Web/GiveMeMoney.py
+++ Web/GiveMeMoney.py
@@ -99,7 +99,7 @@ beneficios_form = form.Form(
 registro_form = form.Form(
 	form.Textbox ('nombre', form.notnull, description = 'Nombre'), 
 	form.Textbox ('apellidos', form.notnull, description = 'Apellidos'),
-	form.Textbox ('email', form.notnull, description = 'E-mail'),
+	form.Textbox ('email', form.notnull, form.regexp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$', 'Formato de correo electronico incorrecto'), description = 'E-mail'),
 	form.Dropdown("dia", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31], description="Dia de nacimiento"),
 	form.Dropdown("mes", ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'], description="Mes de nacimiento"),
 	form.Dropdown("anio", [1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014], description="Anio de nacimiento"),
@@ -173,7 +173,8 @@ class registro:
 									"password": form.d.password
 									}
 			coll.insert(pack_registro)
-		return render.login(usuario = usuario, form = form, mensaje = "Usuario registrado correctamente") 
+			formi = login_form()
+		return render.login(usuario = usuario, form = formi, mensaje = "Usuario registrado correctamente") 
 
 ## Class gastos ##
 class gastos:
